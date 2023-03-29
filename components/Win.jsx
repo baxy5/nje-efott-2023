f2410459dd28ea78c0ef164b5dceacbd44529c9a
baxy5 <bakos.soda@gmail.com>
2023-03-29 13:08:56 +0200

    fonyeremeny fix

--- components/Win.jsx
+++ components/Win.jsx
@@ -4,7 +4,9 @@ const Win = ({ prizes, prizeIndex, restartNext }) => {
   return (
     <div className="absolute lost-background top-0 left-0 w-full h-full flex justify-center items-center">
       <div className="bg-[#CEB239] text-white p-24 rounded-md text-center">
-        <h1 className="text-[64px] pb-10">Nyeremény: {prizes[prizeIndex]}</h1>
+        <h1 className="text-[64px] pb-10">
+          Nyeremény: {prizes[prizeIndex - 1]}
+        </h1>
         <p className="font-bold text-2xl">
           Gratulálunk meg nyertétek a fődíjat!
         </p>
